4b89f9db17c7650590b03492bdf2f4627c153dbe
marcoantoniomejia <marcoantonio.mejialara@gmail.com>
2025-10-07 00:09:40 +0000

    Fix: Replace unsupported container.nodeService role with recommended roles

--- environments/qa/iam.tf
+++ environments/qa/iam.tf
@@ -6,8 +6,20 @@ resource "google_project_iam_member" "gke_service_agent" {
   member  = "serviceAccount:service-${data.google_project.project.number}@container-engine-robot.iam.gserviceaccount.com"
 }
 
-resource "google_project_iam_member" "gke_node_service_agent" {
+resource "google_project_iam_member" "gke_node_logging_writer" {
   project = var.gke_network_project_id
-  role    = "roles/container.nodeService"
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${module.gke_node_sa.email}"
+}
+
+resource "google_project_iam_member" "gke_node_monitoring_writer" {
+  project = var.gke_network_project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${module.gke_node_sa.email}"
+}
+
+resource "google_project_iam_member" "gke_node_monitoring_viewer" {
+  project = var.gke_network_project_id
+  role    = "roles/monitoring.viewer"
   member  = "serviceAccount:${module.gke_node_sa.email}"
 }
